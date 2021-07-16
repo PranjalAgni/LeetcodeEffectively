a2aa8cd91c6c9f8e7cd5b578ba6dfb7ad4316500
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-07-16 23:59:29 +0530

    :sparkles: refactored code

--- Greedy/valid-starting-city.cpp
+++ Greedy/valid-starting-city.cpp
@@ -7,6 +7,7 @@ int validStartingCity(vector<int> distances, vector<int> fuel, int mpg) {
     int currentFuel = 0;
     int startCity = 0;
     int N = distances.size();
+
     for (int idx = 0; idx < N; idx++) {
         int maxMilesCanTravel = currentFuel + fuel[idx] * mpg;
         currentFuel = maxMilesCanTravel - distances[idx];
