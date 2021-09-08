515e59b6ecace060e2c5942364e3932b409ef335
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-09-08 22:32:19 +0530

    :seedling: solved maximum matrix sum medium problem

---
+++ Arrays/maximum-matrix-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        int negativeCount = 0;
+        long long sum = 0;
+        int minNumber = INT_MAX;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                int curr = matrix[row][col];
+                if (curr < 0) negativeCount += 1;
+                sum += abs(curr);
+                minNumber = min(minNumber, abs(curr));
+            }
+        }
+
+        return (negativeCount % 2 == 0) ? sum : sum - (2 * minNumber);
+    }
+};
