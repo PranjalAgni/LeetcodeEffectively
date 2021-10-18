21a85c9ea3be8013dc98935ac9d5cec73bccd208
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-10-19 00:08:16 +0530

    :zap: refactor: refactored common-word

--- Strings/common-word.cpp
+++ Strings/common-word.cpp
@@ -1,26 +1,43 @@
-int solve(string s0, string s1) {
-    unordered_map<string, int> hashMap;
-    int N = s0.length();
+string toLowerCase(string word) {
+    transform(word.begin(), word.end(), word.begin(), ::tolower);
+    return word;
+}
+
+vector<string> convertStringToList(string word) {
+    int N = word.length();
+    vector<string> wordsList;
     string current = "";
+
     for (int idx = 0; idx < N; idx++) {
-        char ch = s0[idx];
+        char ch = word[idx];
         if (ch == ' ' || idx == N - 1) {
-            hashMap[current] += 1;
+            if (idx == N - 1) current += ch;
+            wordsList.push_back(current);
             current = "";
         } else {
             current += ch;
         }
     }
 
+    return wordsList;
+}
+
+int solve(string s0, string s1) {
+    s0 = toLowerCase(s0);
+    s1 = toLowerCase(s1);
+    vector<string> s0List = convertStringToList(s0);
+    vector<string> s1List = convertStringToList(s1);
+    unordered_map<string, int> hashMap;
     int answer = 0;
-    N = s1.length();
-    for (int idx = 0; idx < N; idx++) {
-        char ch = s1[idx];
-        if (ch == ' ' || idx == N - 1) {
-            if (hashMap[current] > 0) answer += 1;
-            current = "";
-        } else {
-            current += ch;
+
+    for (string word : s0List) {
+        hashMap[word] += 1;
+    }
+
+    for (string word : s1List) {
+        if (hashMap[word] > 0) {
+            answer += 1;
+            hashMap[word] = 0;
         }
     }
 
