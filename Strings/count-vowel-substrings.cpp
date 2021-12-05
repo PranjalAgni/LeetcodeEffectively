6c49f9b5e6dc56f7246af55306a4b74dc12f0bf2
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-12-05 13:30:46 +0530

    :rocket: efficient solution for count vowel substrings

--- Strings/count-vowel-substrings.cpp
+++ Strings/count-vowel-substrings.cpp
@@ -5,6 +5,7 @@ public:
     }
     
     // brute force solution
+    // Time: O(N * N) | Space: O(N)
     int countVowelSubstrings(string word) {
         int N = word.length();
         int answer = 0;
@@ -18,5 +19,37 @@ public:
         }
         
         return answer;
+    }
+
+    // efficient solution using sliding window
+    int atMost(string& word, int goal) {
+      int N = word.length();
+      int left = 0, right = 0;
+      int answer = 0;
+      unordered_map<char, int> freqMap;
+      
+      for (int right = 0; right < N; right++) {
+        if (!isVowel(word[right])) {
+          left = right + 1;
+          freqMap.clear();
+          continue;
+        }
+        
+        freqMap[word[right]] += 1;
+        while (freqMap.size() > goal) {
+          freqMap[word[left]] -= 1;
+          if (freqMap[word[left]] == 0) freqMap.erase(word[left]);
+          left += 1;
+        }
+        
+        answer += right - left + 1;
+      }
+      
+      return answer;
+    }
+
+    // Time: O(N) | Space: O(N)
+    int countVowelSubstrings(string word) {
+       return atMost(word, 5) - atMost(word, 4);
     }
 };
