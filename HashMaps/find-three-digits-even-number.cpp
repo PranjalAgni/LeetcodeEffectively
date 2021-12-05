72f022d32bf356756618cd3a0979929563732ea3
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-12-06 00:02:00 +0530

    :sparkles: nice hashmaps easy question

---
+++ HashMaps/find-three-digits-even-number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+  unordered_map<int, int> getFreqMap(vector<int>& digits) {
+    unordered_map<int, int> freqMap;
+    for (int elt: digits) {
+      freqMap[elt] += 1;
+    }
+    
+    return freqMap;
+  }
+  
+  bool check(int elt, unordered_map<int, int> freqMap) {
+    while (elt) {
+      int digit = elt % 10;
+      if (freqMap[digit] <= 0) return false;
+      freqMap[digit] -= 1;
+      elt /= 10;
+    }
+    
+    return true;
+  }
+  
+public:
+    // Time: O(N) | Space: O(N)
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        vector<int> answer;
+        unordered_map<int, int> freqMap = getFreqMap(digits);
+        for (int elt = 100; elt <= 998; elt += 2) {
+          if (check(elt, freqMap)) answer.push_back(elt);  
+        }
+      
+        return answer;
+    }
+};
