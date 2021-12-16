c37a771ee17e4487aba54e48886c1de8d4e249af
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-12-17 00:39:09 +0530

    :green_heart: overlapping intervals problem

--- Greedy/contained-intervals.cpp
+++ Greedy/contained-intervals.cpp
@@ -1,15 +1,17 @@
 bool comparatorFn(vector<int>& intervalA, vector<int>& intervalB) {
-    return intervalA[0] < intervalB[0];
+    return (intervalA[1] == intervalB[1]) ? (intervalA[0] > intervalB[0]) : intervalA[1] < intervalB[1];
 }
 
+// Time: O(N * log(N)) | Space: O(1)
 bool solve(vector<vector<int>>& intervals) {
-    sort(intervals.begin(), intervals.end(), comparatorFn());
     int N = intervals.size();
+    if (N == 1) return false;
+    sort(intervals.begin(), intervals.end(), comparatorFn);
     for (int idx = 1; idx < N; idx++) {
         vector<int>&  intervalA = intervals[idx - 1];
         vector<int>& intervalB = intervals[idx];
-        if ()
+        if ((intervalA[0] >= intervalB[0] && intervalA[1] <= intervalB[1])) return true;
     }
-
+    
     return false;
 }
