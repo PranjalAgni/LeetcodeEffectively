f7a8127ca9ab68b6e92f7e8578b87ca0f6c97ddc
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-03-01 00:15:23 +0530

    :rocket: solved summary ranges

--- Strings/summary-ranges.cpp
+++ Strings/summary-ranges.cpp
@@ -1,26 +1,34 @@
+// https://leetcode.com/problems/summary-ranges/
+
 class Solution {
 public:
+    // Time: O(N) | Space: O(N)
+    // N = length of list
     vector<string> summaryRanges(vector<int>& nums) {
       int N = nums.size();
+      if (N == 0) return {};
       int start = 1;
       int end = N;
       int left = 0;
-      int orignalLeft = left; 
       vector<string> answer;
+      string current = to_string(nums[left]);
+      int incrementBy = 1;
+      
       while (start < end) {
-        if (nums[left] + 1 == nums[start]) {
-          left += 1;
-        } else {
-          string current = to_string(nums[orignalLeft]) + "->" + to_string(nums[start - 1]);
+        if (nums[left] + incrementBy != nums[start]) {
+          if (nums[left] != nums[start - 1]) current += "->" + to_string(nums[start - 1]);
           answer.push_back(current);
           left = start;
-          orignalLeft = left;
+          current = to_string(nums[left]);
+          incrementBy = 0;
         }
+        
         start += 1;
+        incrementBy += 1;
       }
       
       
-      string current = to_string(nums[orignalLeft]) + "->" + to_string(nums[start - 1]);
+      if (nums[left] != nums[start - 1]) current += "->" + to_string(nums[start - 1]);
      
       answer.push_back(current);
       return answer;
