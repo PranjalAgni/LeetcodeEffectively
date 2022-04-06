fc239a76739e5cdc8d143c5fdb82c5b2214bcbe2
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-04-06 23:46:19 +0530

    :potted_plant: solved three sum with multiples

---
+++ Arrays/3-sum-multiples.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    // Time: O(N * N) | Space: O(N * N)
+    int threeSumMulti(vector<int>& arr, int target) {
+        int N = arr.size();
+        int ans = 0;
+        long long MOD = 1e9 + 7;
+        unordered_map<int, int> hashMap;
+        for (int idx = 0; idx < N; idx++) {
+          ans += hashMap[target - arr[idx]] % MOD;
+          for (int jdx = 0; jdx < idx; jdx++) hashMap[arr[idx] + arr[jdx]] += 1;
+        }
+      
+        return ans;
+    }
+};
