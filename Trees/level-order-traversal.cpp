e01f95ef47628493e56e9d7ccb43d1310cf3da0a
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-07-14 00:10:14 +0530

    :memo: solution with only single queue (level order traversal)

--- Trees/level-order-traversal.cpp
+++ Trees/level-order-traversal.cpp
@@ -30,4 +30,34 @@ public:
       
       return answer; 
     }
+
+    // With only single queue used
+    // Time: O(N) | Space: O(N)
+    // N = number of nodes
+    vector<vector<int>> levelOrder(TreeNode* root) {
+      if (!root) return {};
+      queue<TreeNode*> nodesQueue;
+      nodesQueue.push(root);
+      vector<vector<int>> answer;
+      
+      while (!nodesQueue.empty()) {
+        vector<int> currentLevel;
+        int size = nodesQueue.size();
+        
+        while (size-- > 0) {
+          TreeNode* node = nodesQueue.front();
+          nodesQueue.pop();
+          if (node->left) nodesQueue.push(node->left);
+          if (node->right) nodesQueue.push(node->right);
+          currentLevel.push_back(node->val);
+        }
+        
+        if (currentLevel.size() > 0) {
+          answer.push_back(currentLevel);
+        }
+      }
+      
+      return answer;
+      
+    }
 };
