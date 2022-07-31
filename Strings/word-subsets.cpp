97b01792c9dedafbbc1f5c1d17799e2f57b49428
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-07-31 16:11:13 +0530

    :rainbow: solved word-subsets

--- Strings/word-subsets.cpp
+++ Strings/word-subsets.cpp
@@ -2,52 +2,39 @@
 
 class Solution {
 private:
-    bool isSubstring(string& a, string& b, unordered_map<char, int> hashMap) {
-      int N = a.length();
-      int M = b.length();
-      if (M > N) return false;
-      
-      for (char ch: b) {
-        hashMap[ch] -= 1;
-        if (hashMap[ch] < 0) return false;
+    unordered_map<char, int> count(string& word) {
+      unordered_map<char, int> hashMap;
+      for (char& ch: word) {
+        hashMap[ch] += 1;
       }
       
-      return true;
-      
-    }
-  
-    bool isUniversalString(string& word, vector<string>& words, unordered_map<char, int> hashMap) {
-      for (string& subword: words) {
-        if (!isSubstring(word, subword, hashMap)) return false;
-      }
-      
-      return true;
-    }
-  
-    vector<unordered_map<char, int>> preprocess(vector<string>& words) {
-      vector<unordered_map<char, int>> hashMapList;
-      for (string& word: words) {
-        unordered_map<char, int> hashMap;
-        for (char& ch: word) {
-          hashMap[ch] += 1;
-        }
-        hashMapList.push_back(hashMap);
-      }
-      
-      return hashMapList;
+      return hashMap;
     }
 public:
+    // Time: O(N) | Space: O(26)
+    // N = max(len(words1), len(words2))
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        vector<string> answer;
-        vector<unordered_map<char, int>> hashMapList = preprocess(words1);
-        int idx = 0;
-        for (string& word: words1) {
-          if (isUniversalString(word, words2, hashMapList[idx])) {
-            answer.push_back(word);
+        unordered_map<char, int> freqMap;
+        for (string& word: words2) {
+          unordered_map<char, int> currentMap = count(word);
+          for (char ch = 'a'; ch <= 'z'; ch++) {
+            freqMap[ch] = max(freqMap[ch], currentMap[ch]);
           }
-          
-          idx += 1;
         }
+        
+        vector<string> answer;
+        for (string& word: words1) {
+          unordered_map<char, int> currentMap = count(word);
+          bool isUniversal = true;
+          for (char ch = 'a'; ch <= 'z'; ch++) {
+            if (currentMap[ch] < freqMap[ch]) {
+              isUniversal = false;
+              break; 
+            }
+          }
+          if (isUniversal) answer.push_back(word);
+        }
+      
         return answer;
     }
 };
