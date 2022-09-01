2b00d8c25d8d908562a1eb0639ed145bb58a8661
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-09-02 00:18:29 +0530

    :unicorn: solved count good nodes in binary tree

--- Trees/count-good-nodes-in-binary-tree.cpp
+++ Trees/count-good-nodes-in-binary-tree.cpp
@@ -1,24 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+
 class Solution {
 private:
-   void findGoodNodes(TreeNode* root, int& ans) {
-     if (!root) return;
-     
+   // Time: O(N) | Space: O(H)
+   // N = number of nodes in tree
+   // H = height of the tree
+   int findGoodNodes(TreeNode* root, int maxValue) {
+     if (!root) return 0;
+     int res = root->val >= maxValue ? 1 : 0;
+     res += findGoodNodes(root->left, max(maxValue, root->val));
+     res += findGoodNodes(root->right, max(maxValue, root->val));
+     return res;
    }
 public:
     int goodNodes(TreeNode* root) {
-        int ans = 0;
-        findGoodNodes(root, ans);
-        return ans;
+        return findGoodNodes(root, INT_MIN);
     }
 };
