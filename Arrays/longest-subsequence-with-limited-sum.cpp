cebf4d009181ff281c9b0a4146fe1416849f5bb0
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-12-26 00:25:40 +0530

    :christmas_tree: solved longest array subsequence with constrained sum

--- Arrays/longest-subsequence-with-limited-sum.cpp
+++ Arrays/longest-subsequence-with-limited-sum.cpp
@@ -16,28 +16,17 @@ private:
       
       return prefixSum;
     }
-  
-    vector<int> getSuffixSum(vector<int>& nums) {
-      int N = nums.size();
-      vector<int> suffixSum(N, 0);
-      suffixSum[N - 1] = nums[N - 1];
-      for (int idx = N - 2; idx >= 0; idx--) {
-        suffixSum[idx] += nums[idx] + suffixSum[idx + 1];
-      }
-      
-      return suffixSum;
-    }
 public:
+    // Time: O(M*log(N)) | Space: O(N)
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         int N = nums.size();
         int M = queries.size();
         vector<int> answer(M);
-      
+        sort(nums.begin(), nums.end());
         vector<int> prefixSum = getPrefixSum(nums);
-        vector<int> suffixSum = getSuffixSum(nums);
-        
-        int ans = 0;
+      
         for (int idx = 0; idx < M; idx++) {
+          int ans = 0;
           int elt = queries[idx];
           // find longest subsequence in prefixSum
           for (int pos = 0; pos < N; pos++) {
@@ -46,12 +35,21 @@ public:
             }
           }
           
-          // find longest subsequence in suffixSum
-          for (int pos = N - 1; pos >= 0; pos--) {
-            if (suffixSum[pos] <= elt) {
-              ans = max(ans, N - pos);
-            }
-          }
+          answer[idx] = ans;
+        }
+
+        for (int idx = 0; idx < M; idx++) {
+          int elt = queries[idx];
+          
+          // binary search to find the answer
+          int ans = upper_bound(prefixSum.begin(), prefixSum.end(), elt) - prefixSum.begin();
+
+          // searching linearly
+          // for (int pos = 0; pos < N; pos++) {
+          //   if (prefixSum[pos] <= elt) {
+          //     ans = max(ans, pos + 1);
+          //   }
+          // }
           
           answer[idx] = ans;
         }
@@ -59,10 +57,3 @@ public:
         return answer;
     }
 };
-
-// 4 9 11 12
-// 12 8 3  1
-
-// 4 5 2 1
-// sum = 0
-// 
