68a3582d328381b73753362dea76ca7875c2f97d
PranjalAgni <pranjal.agnihotri6@gmail.com>
2023-01-30 21:01:42 +0530

    :tada: nice recursion problem for beginners

---
+++ Recursion/nth-tribonacci-number.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/n-th-tribonacci-number/
+
+// Memoization solution
+class Solution {
+private:
+    // Time: O(N) | Space: O(N)
+    int tribonacciRec(int n, vector<int>& dp) {
+        if (dp[n] != -1) return dp[n];
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        dp[n] = tribonacciRec(n - 1, dp) + tribonacciRec(n - 2, dp) + tribonacciRec(n - 3, dp);
+        return dp[n];
+    }
+public:
+    int tribonacci(int n) {
+        vector<int> dp(n + 1, -1);
+        return tribonacciRec(n, dp);
+     }
+};
