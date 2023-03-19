d320a8d8b50a8cd45040837c933e7f094827ceb4
PranjalAgni <pranjal.agnihotri6@gmail.com>
2023-03-19 23:59:20 +0530

    :sparkles: solved trie problem

---
+++ Tries/design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/design-add-and-search-words-data-structure/
+
+class WordDictionary {
+public:
+    /** Initialize your data structure here. */
+    vector<WordDictionary*> children;
+    bool isEndOfWord;
+    WordDictionary() {
+        children = vector<WordDictionary*>(26, NULL);
+        isEndOfWord = false;
+    }
+    
+    /** Adds a word into the data structure. */
+    void addWord(string word) {
+        WordDictionary* current = this;
+        for (char c: word) {
+          if (current->children[c - 'a'] == NULL) {
+              current->children[c - 'a'] = new WordDictionary();
+          }
+          
+          current = current->children[c - 'a'];
+        }
+      
+        current->isEndOfWord = true;
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    bool search(string word) {
+        WordDictionary* current = this;
+        for(int idx = 0; idx < word.length(); idx++) {
+            char curr = word[idx];
+            if (curr == '.') {
+              for (auto child: current->children) {
+                  if (child && child->search(word.substr(idx + 1))) return true;
+              }
+              
+              return false;
+            }
+          
+            if (current->children[curr - 'a'] == NULL) return false;
+            current = current->children[curr - 'a'];
+        }
+      
+        return current && current->isEndOfWord;
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
