df434b24d7b097ec026a6c1bec4b4bc803f5cb94
PranjalAgni <pranjal.agnihotri6@gmail.com>
2023-07-09 01:15:29 +0530

    :sparkling_heart: solved nice array based problem

---
+++ Arrays/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/
+
+class Solution {
+public:
+    // Time: O(N) | Space: O(N)
+    int longestSubarray(vector<int>& nums) {
+      int N = nums.size();
+      int count = 0;
+      vector<int> countList;
+      for (int idx = 0; idx < N; idx++) {
+        if (nums[idx] == 0) {
+          countList.push_back(count);
+          count = 0;
+        } else {
+          count += 1;  
+        }
+      }
+      
+      if (count) countList.push_back(count);
+      
+      int countSize = countList.size();
+      if (countSize == 1) return max(countList[0] - 1, 0);
+      int answer = 0;
+      for (int idx = 1; idx < countSize; idx++) {
+        answer = max(answer, countList[idx] + countList[idx - 1]);
+      }
+      
+      return answer;
+    }
+};
