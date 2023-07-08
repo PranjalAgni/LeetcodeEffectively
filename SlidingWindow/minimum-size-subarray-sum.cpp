0145278a0e3b0b87572bc73ed03df0dcd72c3a49
PranjalAgni <pranjal.agnihotri6@gmail.com>
2023-07-09 00:18:31 +0530

    :rainbow: really nice problem solved in :hospital:

--- SlidingWindow/minimum-size-subarray-sum.cpp
+++ SlidingWindow/minimum-size-subarray-sum.cpp
@@ -1,7 +1,10 @@
 // https://leetcode.com/problems/minimum-size-subarray-sum/
 
+// This solution uses sliding window
+
 class Solution {
 public:
+    // Time: O(N) | Space: O(1)
     int minSubArrayLen(int target, vector<int>& nums) {
         int N = nums.size();
         int minimalLength = INT_MAX;
@@ -10,14 +13,17 @@ public:
         int currentSum = 0;
         while (right < N) {
           currentSum += nums[right];
-          if (currentSum == target) {
+          
+          while (currentSum >= target) {
             int currentLength = right - left + 1;
             minimalLength = min(minimalLength, currentLength);
+            currentSum -= nums[left];
+            left += 1;
           }
-          if (currentSum > target) {
-            // shrink the window
-          }
+          
           right += 1;
         }
+      
+        return minimalLength == INT_MAX ? 0 : minimalLength;
     }
 };
