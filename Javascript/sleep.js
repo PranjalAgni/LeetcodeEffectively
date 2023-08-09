4db64907b4b3000cab3149c773f0dfb9bb7f6893
PranjalAgni <pranjal.agnihotri6@gmail.com>
2023-08-09 21:03:53 +0530

    :sparkles: javascript sleep implementation

---
+++ Javascript/sleep.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/sleep/
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, millis);
+    })
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
