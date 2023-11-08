b7ea8dc962acbda27e2ce96da003a916b408b77b
PranjalAgni <pranjal.agnihotri6@gmail.com>
2023-11-08 23:30:07 +0530

    :sparkles: solved nice sorting greedy problem

---
+++ Sorting/eliminate-maximum-number-of-monsters.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/eliminate-maximum-number-of-monsters/
+
+class Solution {
+public:
+    // Time: O(N*log(N)) | Space: O(N)
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        int N = dist.size();
+        vector<int> arrival(N);
+        for (int idx = 0; idx < N; idx++) {
+          arrival[idx] = (dist[idx] - 1) / speed[idx];
+        }
+      
+        sort(arrival.begin(), arrival.end());
+        
+        for (int idx = 0; idx < N; idx++) {
+          if (idx > arrival[idx]) return idx;
+        }
+      
+        return arrival.size();
+    }
+};
