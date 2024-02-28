6b4ae5e8e69b4e7920e618fd1a560c74d1d5ccb0
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-02-29 00:05:55 +0530

    :sunflower: added comment on the idea behind this problem

--- Trees/find-bottom-left-tree-value.cpp
+++ Trees/find-bottom-left-tree-value.cpp
@@ -1,3 +1,6 @@
+// https://leetcode.com/problems/find-bottom-left-tree-value/
+
+// Idea: We can find the most left element in the tree, by traversing in level order traversal manner but the catch is we need to travel right to left in the tree, the last element which we reach will be the most left node in that given tree.
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
