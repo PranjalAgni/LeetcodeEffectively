6ecdd57d4fd8b3dbf14028191205d9d5ea366746
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-04-08 23:57:57 +0530

    :beach_umbrella: misc problem currently figuring out

---
+++ Adhoc/number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int N = students.size();
+        
+        int pos = 0;
+        
+    }
+};
+
+// stu = 1, 1, 0, 1
+// sand = 0, 1, 0, 1
+
+
+// stu = 1, 1, 1, 0, 0, 1
+// sand = 1, 0, 0, 0, 1, 1
+
+// stu = 1, 1, 0, 0, 1
+// san = 0, 0, 0, 1, 1
+
+// stu = 1, 0, 0, 1, 1
+// san = 0, 0, 0, 1, 1
+
+// stu = 0, 0, 1, 1, 1
+// san = 0, 0, 0, 1, 1
+
+// stu = 0, 1, 1, 1
+// san = 0, 0, 1, 1
+
+// stu = 1, 1, 1
+// san = 0, 1, 1
+
+// 
