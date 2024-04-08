22f6d5a3357dc42c73c96d846e945d321c12511b
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-04-09 00:15:39 +0530

    :beach_umbrella: easy but nice problem to ask someone

--- Adhoc/number-of-students-unable-to-eat-lunch.cpp
+++ Adhoc/number-of-students-unable-to-eat-lunch.cpp
@@ -1,33 +1,35 @@
+// https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/
+
+/* 
+    Result:
+    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Number of Students Unable to Eat Lunch.
+*/
+
 class Solution {
 public:
+    // Time: O(N) | Space: O(1)
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        int N = students.size();
-        
-        int pos = 0;
-        
+      int square = 0;
+      int circular = 0;
+      int N = sandwiches.size();
+      
+      for (int& student: students) {
+        if (student == 0) circular += 1;
+        else square += 1;
+      }
+      
+      int j = 0;
+      for (j = 0; j < N; j++) {
+        int type = sandwiches[j];
+        if (type == 0) {
+          if (circular <= 0) break;
+          circular -= 1;
+        } else {
+          if (square <= 0) break;
+          square -= 1;
+        }
+      }
+      
+      return N - j;
     }
 };
-
-// stu = 1, 1, 0, 1
-// sand = 0, 1, 0, 1
-
-
-// stu = 1, 1, 1, 0, 0, 1
-// sand = 1, 0, 0, 0, 1, 1
-
-// stu = 1, 1, 0, 0, 1
-// san = 0, 0, 0, 1, 1
-
-// stu = 1, 0, 0, 1, 1
-// san = 0, 0, 0, 1, 1
-
-// stu = 0, 0, 1, 1, 1
-// san = 0, 0, 0, 1, 1
-
-// stu = 0, 1, 1, 1
-// san = 0, 0, 1, 1
-
-// stu = 1, 1, 1
-// san = 0, 1, 1
-
-// 
