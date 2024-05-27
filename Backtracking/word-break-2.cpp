7a44674960be9dd2d347b152e5db6be3ccbc8717
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-05-28 00:39:32 +0530

    :green_heart: solved using backtracking nice problem

--- Backtracking/word-break-2.cpp
+++ Backtracking/word-break-2.cpp
@@ -1,3 +1,6 @@
+// https://leetcode.com/problems/word-break-ii/
+
+// Time: O(2^N) | Space: O(N)
 class Solution {
 private:
     unordered_map<string, bool> getWordsMap(vector<string>& wordDict) {
@@ -9,21 +12,28 @@ private:
       return wordMap;
     }
   
-    void helper(string& str, unordered_map<string, bool>& wordMap, int start, int end, string subpart) {
+    void helper(string& str, unordered_map<string, bool>& wordMap, int start, int end, string subpart, vector<string>& answer) {
       if (end >= str.length()) {
+        if (start == end) {
+          answer.push_back(subpart);
+        }
+        
         return;
       }
       
+      helper(str, wordMap, start, end + 1, subpart, answer); 
       string temp = str.substr(start, end - start + 1);
       if (wordMap.find(temp) != wordMap.end()) {
-        helper(str, wordMap, start, end + 1, subpart + " " + temp);
-      }
+        if (subpart.length() > 0) subpart += " ";
+        helper(str, wordMap, end + 1, end + 1, subpart + temp, answer);
+      }   
     }
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         vector<string> answer;
         string current = "";
         unordered_map<string, bool> wordMap = getWordsMap(wordDict);
-        helper(s, wordMap, 0, 0, "");
+        helper(s, wordMap, 0, 0, "", answer);
+        return answer;
     }
 };
