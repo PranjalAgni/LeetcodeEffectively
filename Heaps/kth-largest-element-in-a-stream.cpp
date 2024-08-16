1cf07194cdfc1d74186edb5e722e3a6c5a350ec7
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-08-16 22:53:48 +0530

    :rocket: good to solve heap based problem

---
+++ Heaps/kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+// TC: O(max(n, k)logk) | Space: O(k)
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int size;
+    KthLargest(int k, vector<int>& nums) {
+        size = k;
+        for (int idx = 0; idx < nums.size(); idx++) {
+            pq.push(nums[idx]);
+            if (pq.size() > k) pq.pop();
+        }
+    }
+  
+    int add(int val) {
+        pq.push(val);
+        if (pq.size() > size) pq.pop();
+      
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
+
+
