6a968b705859064b0c70bfd54087d56faf9c472c
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2024-10-10 23:59:17 +0530

    :rainbow: nice to solve stack problem

---
+++ Stacks/maximum-width-ramp.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/maximum-width-ramp/submissions/
+
+// Good problem to solve based on decreasing stack concept
+// TC: O(N) | Space: O(N)
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        int N = nums.size();
+        // build a decreasing stack
+        
+        stack<int> st;
+        for (int idx = 0; idx < N; idx++) {
+            if (st.empty() || nums[st.top()] > nums[idx]) {
+                st.push(idx);
+            }
+        }
+        
+        int maxWidth = 0;
+        
+        for (int idx = N - 1; idx >= 0; idx--) {
+            while (!st.empty() && nums[st.top()] <= nums[idx]) {
+                maxWidth = max(maxWidth, idx - st.top());
+                st.pop();
+            }
+        }
+        
+        
+        return maxWidth;
+    }
+};
