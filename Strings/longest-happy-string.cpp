96f46b189e544749cb6d7b15829a2e3cf7648d5e
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2024-10-16 23:59:37 +0530

    :thinking: nice string problem currently wip

---
+++ Strings/longest-happy-string.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/longest-happy-string/
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        /**
+        intution
+        maxLen = a + b + c
+        we need to divide the occurences and place such that triplets of same chars are not formed
+        **/
+    }
+};
