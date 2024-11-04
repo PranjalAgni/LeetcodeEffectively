b466819e6bc2f146a6f47917afcee403fa6e63bf
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2024-11-05 01:33:34 +0530

    :sparkles: solved nice string problem

--- Strings/rotate-strings.cpp
+++ Strings/rotate-strings.cpp
@@ -1,38 +1,29 @@
 // https://leetcode.com/problems/rotate-string/
 
+// Time: O(N*N) | Space: O(1)
+// Nice space efficient technique
 class Solution {
+private:
+    bool rotateCheck(string& s, string& goal, int rot) {
+        int N = s.length();
+        for (int idx = 0; idx < N; idx++) {
+            if (s[idx] != goal[(idx + rot) % N]) return false;
+        }
+        
+        return true;
+    }
 public:
     bool rotateString(string s, string goal) {
         int N = s.length();
         int M = goal.length();
         if (N != M) return false;
         
-        int startPos = 0;
-        bool isFound = false;
-        while (startPos < N) {
-            if (s[startPos] == goal[0]) {
-                isFound = true;
-                break;
+        for (int rot = 0; rot < N; rot++) {
+            if (rotateCheck(s, goal, rot)) {
+                return true;
             }
-            startPos += 1;
         }
         
-        if (!isFound) return false;
-        
-        int pos = 0;
-        while (startPos < N) {
-            if (s[startPos] != goal[pos]) return false;
-            startPos += 1;
-            pos += 1;
-        }
-        
-        int x = 0;
-        while (x < startPos && pos < M) {
-            if (s[x] != goal[pos]) return false;
-            x += 1;
-            pos += 1;
-        }
-        
-        return true;
+        return false;
     }
 };
