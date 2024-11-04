33f09d8ca27e3f8458086e459e2c44ab72541d12
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2024-11-04 23:59:06 +0530

    :bulb: currently wip solving it

--- Strings/rotate-strings.cpp
+++ Strings/rotate-strings.cpp
@@ -3,6 +3,36 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
+        int N = s.length();
+        int M = goal.length();
+        if (N != M) return false;
         
+        int startPos = 0;
+        bool isFound = false;
+        while (startPos < N) {
+            if (s[startPos] == goal[0]) {
+                isFound = true;
+                break;
+            }
+            startPos += 1;
+        }
+        
+        if (!isFound) return false;
+        
+        int pos = 0;
+        while (startPos < N) {
+            if (s[startPos] != goal[pos]) return false;
+            startPos += 1;
+            pos += 1;
+        }
+        
+        int x = 0;
+        while (x < startPos && pos < M) {
+            if (s[x] != goal[pos]) return false;
+            x += 1;
+            pos += 1;
+        }
+        
+        return true;
     }
 };
