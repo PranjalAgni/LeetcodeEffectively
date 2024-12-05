7933a61cd766b78b26d91531f595ccc5348f39fd
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2024-12-06 00:44:39 +0530

    :sparkles: a nice string based problem to attempt

--- Strings/move-pieces-to-obtain-a-string.cpp
+++ Strings/move-pieces-to-obtain-a-string.cpp
@@ -1,19 +1,32 @@
-// https://leetcode.com/problems/move-pieces-to-obtain-a-string/?envType=daily-question&envId=2024-12-05
+// https://leetcode.com/problems/move-pieces-to-obtain-a-string/
+
+// Time: O(N) | Space: O(1)
 class Solution {
 public:
     bool canChange(string start, string target) {
         int N = start.length();
         int M = target.length();
+        if (N != M) return false;
+        int spos = 0;
+        int tpos = 0;
         
-        return N == M;
+        while (spos < N || tpos < N) {
+            while(spos < N && start[spos] == '_') spos += 1;
+            while(tpos < N && target[tpos] == '_') tpos += 1;
+            
+            if (spos == N || tpos == N) {
+                break;
+            }
+            
+            if (start[spos] != target[tpos]) return false;   
+            if (start[spos] == 'L' && spos < tpos) return false;
+            if (start[spos] == 'R' && spos > tpos) return false;
+            
+            spos += 1;
+            tpos += 1;
+        }
+        
+        return spos == N && tpos == N;
+        
     }
 };
-
-/**
-
-_L__R__R_
-L______RR
-
-_RL_ 
-_LR_
-**/
