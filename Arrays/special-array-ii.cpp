5818af283bbd3c1b9372b2687a92ff49fb440500
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2024-12-09 23:57:22 +0530

    currently wip

---
+++ Arrays/special-array-ii.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/special-array-ii/
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        int N = nums.size();
+        return {};
+    }
+};
