3b62e00b2ca404d3f265e2328b9ec0116a75b651
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2024-12-23 23:58:19 +0530

    :sparkles: currently wip need to figure out the approach

---
+++ Trees/minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/minimum-number-of-operations-to-sort-a-binary-tree-by-level/?envType=daily-question&envId=2024-12-23
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minimumOperations(TreeNode* root) {
+        
+    }
+};
+
+// Need to figure out how we can identify min number of swaps to make level sorted
