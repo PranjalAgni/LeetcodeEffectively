2cec80bd62086596f8bf036edaf6b63469abd788
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-01-25 00:10:19 +0530

    solved this easy peasy problem

--- Strings/count-prefix-and-suffix-pairs-i.cpp
+++ Strings/count-prefix-and-suffix-pairs-i.cpp
@@ -1,16 +1,44 @@
 // https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/
 
+// Time: O(N^2) | Space: O(1)
+
 class Solution {
+private:
+    bool checkPrefix(string& a, string& b) {
+        int N = a.length();
+        int M = b.length();
+        if (N > M) return false;
+
+        for (int idx = 0; idx < N; idx++) {
+            if (a[idx] != b[idx]) return false;
+        }
+
+        return true;
+    }
+
+    bool checkSuffix(string& a, string& b) {
+        int N = a.length();
+        int M = b.length();
+        if (N > M) return false;
+
+        for (int pos = 0, idx = M - N; idx < M; pos++, idx++) {
+            if (a[pos] != b[idx]) return false;
+        }
+
+        return true;
+    }
 public:
     int countPrefixSuffixPairs(vector<string>& words) {
         int N = words.size();
+        int answer = 0;
         for (int idx = 0; idx < N; idx++) {
+            string a = words[idx];
             for (int jdx = idx + 1; jdx < N; jdx++) {
-                // compare idx and jdx
-                // increment answer
+                string b = words[jdx];
+                if (checkPrefix(a, b) && checkSuffix(a, b)) answer += 1;
             }
         }
 
-        return 42;
+        return answer;
     }
 };
