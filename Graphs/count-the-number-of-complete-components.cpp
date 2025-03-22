c05f12ed40e3a5becb4a8dc4d6704dc518ea6bc7
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-03-23 00:27:49 +0530

    :rocket: passes but still can improve speed

--- Graphs/count-the-number-of-complete-components.cpp
+++ Graphs/count-the-number-of-complete-components.cpp
@@ -1,44 +1,37 @@
 // https://leetcode.com/problems/count-the-number-of-complete-components/
 
 // Time: O(N + E) | Space: O(N)
+// this solution uses set to check if direct edge exists later when checking for complete connected components
 class Solution {
   private:
-      unordered_map<int, vector<int>> constructGraph(vector<vector<int>>& edges) {
-          unordered_map<int, vector<int>> graph;
+      unordered_map<int, unordered_set<int>> constructGraph(vector<vector<int>>& edges) {
+          unordered_map<int, unordered_set<int>> graph;
           for (vector<int>& edge: edges) {
-              graph[edge[0]].push_back(edge[1]);
-              graph[edge[1]].push_back(edge[0]);
+              graph[edge[0]].insert(edge[1]);
+              graph[edge[1]].insert(edge[0]);
           }
   
           return graph;
       }
   
-      void dfs(unordered_map<int, vector<int>>& graph, vector<int>& visited, int node, vector<int>& vertexCovered) {
+      void dfs(unordered_map<int, unordered_set<int>>& graph, vector<int>& visited, int node, vector<int>& vertexCovered) {
           if (visited[node] == 1) return;
           visited[node] = 1;
           vertexCovered.push_back(node);
   
-          for (int& neighbour: graph[node]) {
-              dfs(graph, visited, neighbour, vertexCovered);
+          for (auto it = graph[node].begin(); it != graph[node].end(); it++) {
+              dfs(graph, visited, *it, vertexCovered);
           }
       }
   
-      bool isComplete(unordered_map<int, vector<int>>& graph, vector<int>& vertexCovered) {
+      bool isComplete(unordered_map<int, unordered_set<int>>& graph, vector<int>& vertexCovered) {
           int N = vertexCovered.size();
           for (int idx = 0; idx < N - 1; idx++) {
               int curr = vertexCovered[idx];
-              vector<int> edges = graph[curr];
+              unordered_set<int> edges = graph[curr];
               for (int jdx = idx + 1; jdx < N; jdx++) {
                   int target = vertexCovered[jdx];
-                  bool isFound = false;
-                  for (int& e: edges) {
-                      if (e == target) {
-                          isFound = true;
-                          break;
-                      }
-                  }
-  
-                  if (!isFound) return false;
+                  if (edges.find(target) == edges.end()) return false;
               }
           }
   
@@ -46,14 +39,10 @@ class Solution {
       }
   public:
       int countCompleteComponents(int N, vector<vector<int>>& edges) {
-          // we need to construct a graph
-          // dfs on graph 
-          // count how many times we need to do the root dfs()
-          // return the count
-          unordered_map<int, vector<int>> graph = constructGraph(edges);
-  
+          unordered_map<int, unordered_set<int>> graph = constructGraph(edges);
           vector<int> visited(N, 0);
           int answer = 0;
+  
           for (int vertex = 0; vertex < N; vertex++) {
               vector<int> vertexCovered;
               if (visited[vertex] == 0) {
