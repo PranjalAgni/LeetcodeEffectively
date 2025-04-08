5bb35a85770d3b8850ac8a614ddd3f5fd692b588
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-04-08 23:59:49 +0530

    :sparkles: currently solving this adhoc problem

---
+++ Adhoc/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/minimum-number-of-operations-to-make-elements-in-array-distinct/?envType=daily-question&envId=2025-04-08
+
+class Solution {
+  public:
+      int minimumOperations(vector<int>& nums) {
+          // how can we solve this problem
+          // removing the first 3 everytime I find a dup
+          // make a freqmap for elements
+          // if dup is found remove the first 3 then continue
+      }
+  };
