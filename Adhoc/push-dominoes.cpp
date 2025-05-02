3169eb1984f5786f51a27e83d8db108eedfd9c6c
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-05-02 23:56:18 +0530

    :thinking: nice problem to solve

---
+++ Adhoc/push-dominoes.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/push-dominoes/?envType=daily-question&envId=2025-05-02
+class Solution {
+  public:
+      string pushDominoes(string dominoes) {
+          // simulate and find the answer\
+          return dominoes;
+      }
+  };
