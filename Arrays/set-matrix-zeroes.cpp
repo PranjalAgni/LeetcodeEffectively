3e92ec311d62029144ad53679a4b7d91b51ddb72
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-05-23 00:25:45 +0530

    :sparkles: good demo today + nice problem solved

--- Arrays/set-matrix-zeroes.cpp
+++ Arrays/set-matrix-zeroes.cpp
@@ -1,4 +1,6 @@
 // https://leetcode.com/problems/set-matrix-zeroes/?envType=daily-question&envId=2025-05-21
+
+// Time: O(n^2) | Space: O(1)
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
@@ -6,11 +8,29 @@ public:
         int cols = matrix[0].size();
 
         vector<vector<int>> positions;
+        
+        // pre processing
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 if (matrix[row][col] == 0) {
                     positions.push_back({row, col});
                 }
+            }
+        }
+
+        // core logic
+        for (vector<int> position: positions) {
+            int row = position[0];
+            int col = position[1];
+
+            // set to zero horizontally
+            for (int c = 0; c < cols; c++) {
+                matrix[row][c] = 0;
+            }
+
+            // set to zero vertically
+            for (int r = 0; r < rows; r++) {
+                matrix[r][col] = 0;
             }
         }
     }
