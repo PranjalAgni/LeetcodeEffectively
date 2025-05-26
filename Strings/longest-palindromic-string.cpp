2f8b473384402f3dc363d0f4ba136d19c1dd7cb7
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-05-27 00:53:21 +0530

    :zap: nice and intutive string + hashmap based problem

--- Strings/longest-palindromic-string.cpp
+++ Strings/longest-palindromic-string.cpp
@@ -1,34 +1,35 @@
+// https://leetcode.com/problems/longest-palindrome-by-concatenating-two-letter-words/
+
+// Time: O(N*len(word)) | Space: O(N)  
+// N = number of words
+
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
         int N = words.size();
         unordered_map<string, int> freqMap;
-        for (int idx = 0; idx < N; idx++) {
-            freqMap[words[idx]] += 1;
+        for (string& word: words) {
+            freqMap[word] += 1;
         }
-
+        
         int answer = 0;
-
-        for (int idx = 0; idx < N; idx++) {
-            string word = words[idx];
+        int middlePlacement = 0;
+        for (const auto& [word, freq]: freqMap) {
             string reverseWord = word;
             reverse(reverseWord.begin(), reverseWord.end());
             
             if (word == reverseWord) {
-                answer += freqMap[word];
-                freqMap[word] = 0;
-            } else {
-                int a = freqMap[word];
+                answer += (freq / 2) * 4;
+                if (freq % 2 == 1) middlePlacement = 1;
+            } else if (word < reverseWord && freqMap.count(reverseWord))  {
+                // this else-if prevents counting reverseWord twice
+                int a = freq;
                 int b = freqMap[reverseWord];
-
-                int minPairs = min(a, b);
-                freqMap[word] -= minPairs;
-                freqMap[reverseWord] -= minPairs;
-                answer += minPairs;
+                answer +=  min(a, b) * 4;
             }
         }
 
 
-        return answer * 2;
+        return answer + middlePlacement * 2;
     }
 };
