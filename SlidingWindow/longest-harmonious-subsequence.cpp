664f097a060c2f578218f80895a0c09ad6e58e3b
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-06-30 23:59:32 +0530

    :sparkles: currently wip

---
+++ SlidingWindow/longest-harmonious-subsequence.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/longest-harmonious-subsequence/
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        int N = nums.size();
+
+        for (int idx = 1; idx < N; idx++) {
+            int diff = nums[idx] - nums[idx - 1];
+            
+        }
+     }
+};
