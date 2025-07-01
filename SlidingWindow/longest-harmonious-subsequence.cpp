46e58397d52e4b2a857bfe73b310c8c7368485b5
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-07-01 18:41:54 +0530

    :bulb: not exactly sliding window but a IQ test

--- SlidingWindow/longest-harmonious-subsequence.cpp
+++ SlidingWindow/longest-harmonious-subsequence.cpp
@@ -1,12 +1,27 @@
 // https://leetcode.com/problems/longest-harmonious-subsequence/
+
+// this problem is like a IQ test
+// this is easy but the solution is creative
+// also a reminder to read the problem statement carefully
+
+// Time: O(n) | Space: O(n)
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
         int N = nums.size();
-
-        for (int idx = 1; idx < N; idx++) {
-            int diff = nums[idx] - nums[idx - 1];
-            
+        unordered_map<int, int> freqMap;
+        for (int& num: nums) {
+            freqMap[num] += 1;
         }
-     }
+
+        int answer = 0;
+
+        for (auto const& [key, value]: freqMap) {
+            if (freqMap.find(key + 1) != freqMap.end()) {
+                answer = max(answer, value + freqMap[key + 1]);
+            }
+        }
+
+        return answer;
+    }
 };
