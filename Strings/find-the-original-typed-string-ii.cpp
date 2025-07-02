a9704652f25bbd8d32c3105057cb62d7008acf86
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-07-02 23:59:26 +0530

    :wip: trying to solve a hard lc after long

---
+++ Strings/find-the-original-typed-string-ii.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/find-the-original-typed-string-ii/
+
+class Solution {
+public:
+    int possibleStringCount(string word, int k) {
+        int N = word.length();
+        if (N == k) return 1;
+
+    }
+};
+
+
+// aaabbb
+
+// abb
+// aab
+// abb
+// aaa
+// bbb
+// abbb
+// aabb
+//
