e644e7b414780eb0df172d9eee7220b58004245f
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-11-25 23:58:20 +0530

    :sparkles: trying to solve maths based problem

---
+++ Maths/smallest-integer-divisible-by-k.cpp
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/smallest-integer-divisible-by-k/?envType=daily-question&envId=2025-11-25
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        
+    }
+};
