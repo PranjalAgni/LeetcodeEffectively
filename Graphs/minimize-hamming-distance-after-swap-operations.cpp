91f243829b77d90a9a818dd55bd395270a7150d2
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2026-04-23 23:59:31 +0530

    :hammer: understanding and trying to solve it

---
+++ Graphs/minimize-hamming-distance-after-swap-operations.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+        
+    }
+};
+
+// source = [5,1,2,4,3], target = [1,5,4,2,3], allowedSwaps = [[0,4],[4,2],[1,3],[1,4]]
+
+// hashMap1 = {5: 0, 1: 1, 2: 2, 4: 3, 3: 4}
+// hashMap2 = {1: 0, 5: 1, 4: 2, 2: 3, 3: 4}
+
+// swaps_we_need = [[0, 1], [2,3]]
